1b671f43ce439a238c48cacf6b3c0604423f7a05
Michalis Efstathiou <michalis@users.noreply.github.com>
2019-01-12 23:07:42 +0200

    Added keyup event listener, fixed minor bugs

--- js/script.js
+++ js/script.js
@@ -93,79 +93,105 @@ const appendPageLinks = ( list, itemsPerPage ) => {
 ***/
 
 const search = ( list, itemsPerPage ) => {
+
+  const listItems = list.querySelectorAll('li');
+
   // Create html elements and append to page.
-  const div = document.createElement('div');
-  div.className = 'student-search';
+  const searchDiv = document.createElement('div');
+  searchDiv.className = 'student-search';
   const input = document.createElement('input');
   input.placeholder = 'Search for students..';
-  div.appendChild(input);
+  searchDiv.appendChild(input);
   const button = document.createElement('button');
   button.textContent = 'Search';
-  div.appendChild(button);
+  searchDiv.appendChild(button);
   const listPreviousSibling = list.previousElementSibling;
-  listPreviousSibling.appendChild(div);
+  listPreviousSibling.appendChild(searchDiv);
 
-  // Add functionality to search button.
-  const listItems = list.querySelectorAll('li');
-  const searchDiv = document.querySelector('.student-search');
+  // Event handler for when search button is clicked.
   searchDiv.addEventListener( 'click', event => {
-    if ( event.target.tagName.toLowerCase() == 'button' ) {
-      const userInput = searchDiv.querySelector('input').value;
-      // If search input is empty, give a warning.
-      if ( userInput.length < 1 ) {
-        const warningExists = document.querySelector('.noInput');
-        if ( warningExists === null ) {
-          const noInput = document.createElement('p');
-          noInput.className = 'noInput'
-          noInput.textContent = `The search field is empty,
-                          please provide a search term!`;
-          div.parentNode.appendChild(noInput);
-        }
-      } else {
-        // Remove warning, if it exists.
-        const warningExists = document.querySelector('.noInput');
-        if ( warningExists !== null ) {
-          const parentNode = warningExists.parentNode;
-          parentNode.removeChild(warningExists);
-        }
-        // Create a new element and fill it with list items that contain the search query.
-        const searchResults = document.createElement('ul');
-        searchResults.className = "student-list";
-        let matchedItemsCounter = 0;
-        for ( let i = 0; i < listItems.length; i++ ) {
-          const textContent = listItems[i].textContent;
-          if ( textContent.includes(userInput) ) {
-            searchResults.appendChild( listItems[i] );
-            matchedItemsCounter++;
-          }
-        }
-          // If there are search results
-          if ( matchedItemsCounter > 0 ) {
-            // Remove current list, pagination and search funtion, and rebuild them using search results.
-            const currentList = document.querySelector('.student-list');
-            const currentListParent = currentList.parentNode;
-            currentListParent.removeChild(currentList);
-            const pagination = document.querySelector('.pagination');
-            const paginationParent = pagination.parentNode;
-            paginationParent.removeChild(pagination);
-            currentListParent.appendChild(searchResults);
-            const newStudentList = document.querySelector('.student-list');
-            showPage( newStudentList, itemsPerPage, 1 );
-            appendPageLinks( newStudentList, itemsPerPage );
-          } else {
-            // Let the user know there are no results matching the query.
-            const noMatch = document.querySelector('.noMatch');
-            if ( noMatch === null ) {
-              const noMatch = document.createElement('p');
-              noMatch.className = 'noMatch';
-              noMatch.textContent = `There are no matches,
-                                    please try again!`;
-              div.parentNode.appendChild(noMatch);
-            }
-         }
-      }
+    if ( event.target.tagName === 'BUTTON' ) {
+      searchFunctionality();
     }
   });
+
+  // Event handler for when user is typing.
+  searchDiv.addEventListener('keyup', event => {
+    if ( event.target.tagName === 'INPUT' ) {
+      searchFunctionality();
+    }
+  });
+
+  // Event handler for when the enter key is pressed.
+  searchDiv.addEventListener('keyup', event => {
+    if ( event.target.tagName === 'INPUT' && event.keyCode === 13  ) {
+      searchFunctionality();
+    }
+  });
+
+  // Search Functionality
+  const searchFunctionality = () => {
+    const userInput = searchDiv.querySelector('input').value.toLowerCase();
+    // If search input is empty, give a warning.
+    if ( userInput.length < 1 && event.target.tagName === 'BUTTON' ) {
+      const warningExists = document.querySelector('.noInput');
+      if ( warningExists === null ) {
+        const noInput = document.createElement('p');
+        noInput.className = 'noInput'
+        noInput.textContent = `The search field is empty,
+                        please provide a search term!`;
+        searchDiv.parentNode.appendChild(noInput);
+      }
+    } else {
+      // Remove warning, if it exists.
+      const warningExists = document.querySelector('.noInput');
+      if ( warningExists !== null ) {
+        const parentNode = warningExists.parentNode;
+        parentNode.removeChild(warningExists);
+      }
+      // Create a new element and fill it with list items that contain the search query.
+      const searchResults = document.createElement('ul');
+      searchResults.className = "student-list";
+      let matchedItemsCounter = 0;
+      for ( let i = 0; i < listItems.length; i++ ) {
+        const textContent = listItems[i].textContent.toLowerCase();
+        if ( textContent.includes(userInput) ) {
+          searchResults.appendChild( listItems[i] );
+          matchedItemsCounter++;
+        }
+      }
+        // If there are search results
+        if ( matchedItemsCounter > 0 ) {
+          // Remove warning, if it exists.
+          const noMatch = document.querySelector('.noMatch');
+          if ( noMatch !== null ) {
+            const parentNode = noMatch.parentNode;
+            parentNode.removeChild(noMatch);
+          }
+          // Remove current list, pagination and search funtion, and rebuild them using search results.
+          const currentList = document.querySelector('.student-list');
+          const currentListParent = currentList.parentNode;
+          currentListParent.removeChild(currentList);
+          const pagination = document.querySelector('.pagination');
+          const paginationParent = pagination.parentNode;
+          paginationParent.removeChild(pagination);
+          currentListParent.appendChild(searchResults);
+          const newStudentList = document.querySelector('.student-list');
+          showPage( newStudentList, itemsPerPage, 1 );
+          appendPageLinks( newStudentList, itemsPerPage );
+        } else {
+          // Let the user know there are no results matching the query.
+          const noMatch = document.querySelector('.noMatch');
+          if ( noMatch === null ) {
+            const noMatch = document.createElement('p');
+            noMatch.className = 'noMatch';
+            noMatch.textContent = `There are no matches,
+                                  please try again!`;
+            searchDiv.parentNode.appendChild(noMatch);
+          }
+       }
+    }
+  };
 };
 
 
